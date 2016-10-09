afe8a8e7465888585b0adb4a105d1468f5841c7b
np <papakos@localhost.localdomain>
2016-10-09 13:23:20 +0100

    design done

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ var work     = document.getElementsByClassName('worklist');
 var language = document.getElementsByClassName('languagelist');
 var software = document.getElementsByClassName('softList');
 var proj     = document.getElementsByClassName('projectlist');
+// ------- skills
 var javaProg = document.getElementsByClassName('textJava');
 var cProg    = document.getElementsByClassName('textC');
-var progLang = [javaProg, cProg];
+var htmlProg = document.getElementsByClassName('textHtml');
+var progLang = [javaProg, cProg, htmlProg];
+// ------- spoken languages
+var english  = document.getElementsByClassName('eng');
+var greek    = document.getElementsByClassName('gre');
+var spanish  = document.getElementsByClassName('spa');
+var german    = document.getElementsByClassName('ger');
+var perInfo  = document.getElementsByClassName('personalInfo');
+var spokLang = [english, greek, spanish, german];
 function showClick(className){
   var style = window.getComputedStyle(className[0]);
   if(style.getPropertyValue("display")=="none"){
@@ -15,10 +24,17 @@ function showClick(className){
     className[0].style.display = "none";
   }
 }
-function showSkill(className){
-  for(var i=0; i<progLang.length;i++){
-    console.log(progLang[i]);
-    progLang[i][0].style.display = "none";
-  }
+function showSkill(className,list){
+  closeSkill(list);
   className[0].style.display = "inline-block";
+}
+function closeSkill(list){
+  for(var i=0; i<list.length;i++){
+    console.log(list[i]);
+    list[i][0].style.display = "none";
+  }
+}
+function closeClickSkill(className,list){
+  closeSkill(list);
+  showClick(className);
 }
