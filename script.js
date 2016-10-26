5106b0829b7495c49af110e335b5c65bfcfceded
np <papakos@localhost.localdomain>
2016-10-26 15:13:27 +0100

    mobiling

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ var cProg    = document.getElementsByClassName('textC');
 var htmlProg = document.getElementsByClassName('textHtml');
 var sqlite   = document.getElementsByClassName('textSQL');
 var nodeJS   = document.getElementsByClassName('textJS');
+var matlab   = document.getElementsByClassName('textMAT');
 var python   = document.getElementsByClassName('textPy');
 var cpp      = document.getElementsByClassName('textCpp');
-var progLang = [javaProg, cProg, htmlProg, sqlite, nodeJS, python, cpp];
+var progLang = [javaProg, cProg, htmlProg, sqlite, nodeJS, python, cpp, matlab];
 // ------- spoken languages
 var english  = document.getElementsByClassName('eng');
 var greek    = document.getElementsByClassName('gre');
@@ -37,8 +38,15 @@ function showClick(className){
 function showSkill(className,list){
   closeSkill(list);
   className[0].style.display = "inline-block";
+  // new---start
+  skills[0].style.width="80%";
+  skills[0].style.marginTop="3%";
+  // skills[0].style.float="right";
+  // new --end
 }
 function closeSkill(list){
+  skills[0].style.width="45%";
+  skills[0].style.marginTop="8%";
   for(var i=0; i<list.length;i++){
     console.log(list[i]);
     list[i][0].style.display = "none";
@@ -56,4 +64,37 @@ function whenScrolling(){
     header[0].className ="";
   }
 }
+function whenMobile(){
+  document.getElementById('myAbst').className="myAbst-m";
+  document.getElementsByTagName('body')[0].className="body-m";
+  document.getElementsByTagName('footer')[0].className="footer-m";
+  document.getElementById('edu-entries').className="edu-entries-m";
+  document.getElementById('worklist').className="worklist-m";
+  // var eduEnt = document.getElementsByClassName('edu-entries')[0];
+  // eduEnt.style.width="70%";
+  // var eduLogo = eduEnt.getElementsByClassName('logo');
+  // for(var i=0;i<eduLogo.length;i++){
+  //   eduLogo[i].style.transform="scale(.7)";
+  // }
+
+}
+function whenDesktop(){
+  document.getElementById('myAbst').className="myAbst";
+  document.getElementsByTagName('body')[0].className="";
+  document.getElementsByTagName('footer')[0].className="";
+  document.getElementById('edu-entries').className="edu-entries";
+  document.getElementById('worklist').className="worklist";
+}
+function whenResize(){
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+  if(w<1000) whenMobile();
+  else whenDesktop();
+  // alert("W:"+w+"\n"+"H:"+h);
+  console.log("W:"+w+"\n"+"H:"+h);
+  // console.log("height:"+window.screen.availHeight);
+  // console.log("width:"+window.screen.availWidth);
+}
 window.addEventListener("scroll", whenScrolling);
+window.addEventListener("resize", whenResize);
+whenResize();
